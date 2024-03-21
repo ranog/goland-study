aa39789159ca927edae2442e2cd1c3597f6440b8
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-03-21 10:46:31 -0300

    start project

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type user struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	log.Println("Server started on :8080")
+	mux.HandleFunc("/users", listUsersHandler)
+	log.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func listUsersHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("List users")
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	users := []*user{}
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, &u)
+	}
+
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
